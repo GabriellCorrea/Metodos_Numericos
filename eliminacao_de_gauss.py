5a5c68021cc8192aa6bc4a8605676ce8c8463ca2
Gabriel Corrêa <gabrielcs.1003@gmail.com>
2026-06-15 21:44:39 -0300

    Implementando pivoteamento

--- eliminacao_de_gauss.py
+++ eliminacao_de_gauss.py
@@ -6,12 +6,26 @@ def eliminacao_gauss(A, b):
 
     n = len(b)
 
+    # ELIMINAÇÃO COM PIVOTEAMENTO
     for i in range(n):
+
+        # PIVOTEAMENTO
+        max_linha = np.argmax(abs(A[i:, i])) + i
+
+        if A[max_linha][i] == 0:
+            raise ValueError("Sistema sem solução única")
+
+        # troca linhas
+        A[[i, max_linha]] = A[[max_linha, i]]
+        b[[i, max_linha]] = b[[max_linha, i]]
+
+        # eliminação
         for j in range(i + 1, n):
             fator = A[j][i] / A[i][i]
             A[j] = A[j] - fator * A[i]
             b[j] = b[j] - fator * b[i]
 
+    # SUBSTITUIÇÃO RETROATIVA
     x = np.zeros(n)
 
     for i in range(n - 1, -1, -1):
